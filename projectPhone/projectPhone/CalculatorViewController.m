47c022931cd11afb45561fff8b99af9dfcaae6bc
Avatar <ahmedhachemi1994@gmail.com>
2016-12-19 00:48:59 +0100

    basics need les contraintes

--- projectPhone/projectPhone/CalculatorViewController.m
+++ projectPhone/projectPhone/CalculatorViewController.m
@@ -93,9 +93,49 @@ NSString* (^add) (NSString*, NSString*) = ^(NSString* txt, NSString * nb) {
 
 // Operators
 - (IBAction)equal:(id)sender {
-    int result = 0;
     
+    int result = 0;
+    int a = 0;
+    int b = 0;
+    NSMutableString* operator = [[NSMutableString  alloc] init];
+    NSMutableString* strA = [[NSMutableString  alloc] init];
+    NSMutableString* strB = [[NSMutableString  alloc] init];
+    int i;
+    //char current;
+    unsigned long len = [_op.text length];
+    unsigned short index [len];
+    [_op.text getCharacters:index range:NSMakeRange(0, len)];
+    
+    for(i = 0; i < len; i++) {
+        if(index[i] >= '0' && index[i] <= '9' ) {
+             [strA appendFormat:@"%c",index[i]];
+        } else {
+             [strB appendString:strA];
+             [strA setString:@""];
+             [operator appendFormat:@"%c",index[i]];
+         }
+    }
+    
+    a = [strA intValue];
+    b = [strB intValue];
+    NSLog(@"%d %d",b,a);
+    
+    if ([operator isEqual:@"-"]) {
+        NSLog(@"0");
+        result = b - a;
+    }else if([operator isEqual:@"+"]) {
+        NSLog(@"1");
+        result = b + a;
+    }else if ([operator  isEqual: @"x"]) {
+        NSLog(@"2");
+        result = b * a;
+    }else {
+        NSLog(@"3");
+        result = b / a;
+    }
+    _res.text = [NSString stringWithFormat:@"%d",result];
     _op.text = @"";
+    
 }
 - (IBAction)divisor:(id)sender {
      _op.text = add(_op.text,[sender currentTitle]);
